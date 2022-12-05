938d602290d141121df825446688946a1dc0a290
fmallari82 <fmallari82@gmail.com>
2022-12-04 17:24:21 -0800

    daikichi path variable assignment update

--- src/main/java/com/francisco/daikichiroutes/controllers/DaikichiController.java
+++ src/main/java/com/francisco/daikichiroutes/controllers/DaikichiController.java
@@ -1,5 +1,7 @@
 package com.francisco.daikichiroutes.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,5 +24,17 @@ public class DaikichiController {
 	@RequestMapping("/daikichi/tomorrow")
 	public String tomorrow() {
 		return "Tomorrow, an opportunity will arise, so be sure to be open to new ideas!";
+	}
+	@GetMapping("/daikichi/travel/{destination}")
+	public String eventDestination( @PathVariable("destination") String destination) {
+		return "Congratulations! You will soon travel to " + destination + "!";
+	}
+	@GetMapping("daikichi/lotto/{number}")
+	public String eventDestination(@PathVariable("number") Integer number) {
+		if(number % 2 == 0) {
+			return "You will take a grand journey in the near future, but be weary of tempting offers";			
+		}else {
+			return "You have enjoyed the fruits of your labor but now is a great time to spend time with family and friends";
+		}
 	}
 }
